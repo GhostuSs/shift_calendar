146b9effdd8aad9b199a617bf07f54bb7f3c0f65
Maxim Movchan <maksmovchan@icloud.com>
2022-01-14 18:58:50 +0300

    'assets added'

---
+++ lib/data/res/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract class ProjectColors {
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color darkGray = Color(0xFF353535);
+  static const Color coralRed = Color(0xFFFF1E47);
+  static const Color red = Color(0xFFFF1F00);
+  static const Color lightGray = Color(0xFFF5F5F5);
+  static const Color backgroundGray = Color(0xFFE5E5E5);
+  static const Color black = Colors.black;
+  static const Color orange = Color(0xFFFF9900);
+  static const Color darkBlue = Color(0xFF0038FF);
+  static const Color lightBlue = Color(0xFF3B82F4);
+  static const Color darkGreen = Color(0xFF5AB500);
+  static const Color lightGreen = Color(0xFF70FF00);
+  static const Color purple = Color(0xFF9C00FC);
+
+}
