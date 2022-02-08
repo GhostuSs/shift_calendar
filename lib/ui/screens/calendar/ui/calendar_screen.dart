e552cfd8a83f8332d898af772268d34ef80a3f48
Maxim Movchan <m.movchan@spb.mcart.ru>
2022-02-08 21:16:38 +0300

    'added calendar'

--- lib/ui/screens/calendar/ui/calendar_screen.dart
+++ lib/ui/screens/calendar/ui/calendar_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shift_calendar/ui/res/colors/colors.dart';
 import 'package:shift_calendar/ui/res/uikit/appbars/raw_appbar.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({Key? key}) : super(key: key);
@@ -21,10 +22,37 @@ class _CalendarScreenState extends State<CalendarScreen> {
       ),
       backgroundColor: ProjectColors.white,
       body: Column(
-        children: const [
-          Text(
-            'soon',
-            style: TextStyle(color: Colors.red),
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          TableCalendar(
+            currentDay: DateTime.now(),
+            calendarStyle: const CalendarStyle(
+                markerMargin: EdgeInsets.zero,
+                cellMargin: EdgeInsets.zero,
+                cellPadding: EdgeInsets.zero,
+                canMarkersOverflow: true,
+                disabledTextStyle:
+                    TextStyle(color: ProjectColors.black, fontSize: 17.0),
+                selectedTextStyle:
+                    TextStyle(color: ProjectColors.white, fontSize: 17.0),
+                defaultTextStyle:
+                    TextStyle(color: ProjectColors.black, fontSize: 17.0),
+                outsideTextStyle:
+                    TextStyle(color: ProjectColors.black, fontSize: 17.0),
+                weekendDecoration: BoxDecoration(
+                    shape: BoxShape.rectangle, color: ProjectColors.lightGray),
+                todayDecoration: BoxDecoration(
+                    shape: BoxShape.rectangle, color: ProjectColors.black),
+                selectedDecoration: BoxDecoration(
+                    shape: BoxShape.rectangle, color: ProjectColors.black)),
+            headerVisible: false,
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            rowHeight: 88,
+            focusedDay: DateTime.now(),
+            firstDay: DateTime(2022),
+            lastDay: DateTime(2023),
           )
         ],
       ),
